b58a18fc020e7a0658b079448753e6a320bd0151
Jackson Tucker <jt070017@ohio.edu>
2020-01-21 11:44:58 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Jackson Tucker>
+ *        Date: <1-21-2020>
+ * Description: This simply writes Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
